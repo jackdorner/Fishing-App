3fc094c65c0008185b0df01be49dd1e13a1aad63
jackdorner <jackdorner9@gmail.com>
2025-06-03 23:15:30 -0500

    progress

---
+++ AWS_Resources/removeFish/index.mjs
@@ -0,0 +1,68 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import Ajv from "ajv";
+import addFormats from "ajv-formats";
+
+const ajv = new Ajv();
+addFormats(ajv);
+const sqs = new SQSClient({ region: "us-east-2" });
+const QUEUE_URL = process.env.QUEUE_URL;
+
+const fishSchema = {
+  type: "object",
+  required: ["name", "species", "length_in", "catch_date"],
+  properties: {
+    name: { type: "string" },
+    species: { type: "string" },
+    length_in: { type: "number" },
+    catch_date: { type: "string", format: "date" }
+  },
+  additionalProperties: false
+};
+
+const validate = ajv.compile(fishSchema);
+
+export const handler = async (event) => {
+  console.log("🔹 Event received:", JSON.stringify(event, null, 2));
+
+  let body;
+  try {
+    if (typeof event.body === "string") {
+      body = JSON.parse(event.body);
+    } else {
+      body = event.body;
+    }
+  } catch (err) {
+    console.error("Invalid JSON: ", err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Malformed JSON" }),
+    };
+  }
+
+  if (validate(body)) {
+    try {
+      const result = await sqs.send(new SendMessageCommand({
+        QueueUrl: QUEUE_URL,
+        MessageBody: JSON.stringify(body),
+      }));
+
+      console.log(`✅ message sent to SQS:`, result.MessageId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Fish added" }),
+      };
+    } catch (err) {
+      console.error("🔥 Error sending to SQS:", err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Internal Server Error" }),
+      };
+    }
+  } else {
+    console.log("🔥 Invalid input:", validate.errors);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Input doesn't match the fish schema" }),
+    };
+  }
+};
